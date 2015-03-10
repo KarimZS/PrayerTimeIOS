e4bffe575968e0ca20176188a8240670986da3ac
Karim Soliman <karim@Karims-MacBook-Air.local>
2015-03-09 20:38:45 -0400

    Initial Commit

---
+++ PrayerTime/SecondViewController.swift
@@ -0,0 +1,25 @@
+//
+//  SecondViewController.swift
+//  PrayerTime
+//
+//  Created by Karim Soliman on 3/9/15.
+//  Copyright (c) 2015 Karim Soliman. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
