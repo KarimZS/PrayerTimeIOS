701396f18582aad27ce44ffe0f277793d78d565a
karimsoliman94 <karimsoliman94@hotmail.com>
2016-06-26 17:41:13 -0500

    created

---
+++ PrayerTime/FourthViewController.swift
@@ -0,0 +1,24 @@
+//
+//  FourthViewController.swift
+//  PrayerTime
+//
+//  Created by Karim Soliman on 3/13/15.
+//  Copyright (c) 2015 Karim Soliman. All rights reserved.
+//
+
+import UIKit
+
+class FourthViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
---
+++ PrayerTime/ThirdViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ThirdViewController.swift
+//  PrayerTime
+//
+//  Created by Karim Soliman on 3/12/15.
+//  Copyright (c) 2015 Karim Soliman. All rights reserved.
+//
+
+import UIKit
+
+class ThirdViewController: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    
+}
